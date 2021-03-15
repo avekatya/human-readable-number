fbcb52463a198a2270350da3733d913299703219
AveKatya <katyaverchenko@gmail.com>
2021-03-16 00:37:05 +0300

    feat: implement human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+	if(number === 0) {
+		return "zero";
+	}
+
+	const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+	const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+	let result = "";
+	let hundred = Math.floor(number / 100);
+	let ten = Math.floor((number - hundred * 100) / 10);
+	let unit = number - (hundred * 100) - (ten * 10);
+
+	if(hundred > 0) {
+		result = result.concat(`${units[hundred]} hundred`);
+	}
+
+	if(ten >= 2) {
+		result = result.concat(` ${tens[ten]}`);
+		result = result.concat(` ${units[unit]}`);
+	} else {
+		result = result.concat(` ${units[Number.parseInt('' + ten + unit)]}`);
+	}
+
+	return result.trim();
 }
